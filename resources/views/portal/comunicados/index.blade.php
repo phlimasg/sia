a8ccb9941ca97cb637e927609e78bb8946deee50
phlimasg <raphaelpc_@hotmail.com>
2019-09-03 10:20:40 -0300

    Update index.blade.php

--- resources/views/portal/comunicados/index.blade.php
+++ resources/views/portal/comunicados/index.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
 @php
-  setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+  setlocale(LC_TIME, 'pt-BR', 'pt-BR.utf-8', 'pt-BR.utf-8', 'portuguese');
   date_default_timezone_set('America/Sao_Paulo');
 @endphp
 <div class="row">
