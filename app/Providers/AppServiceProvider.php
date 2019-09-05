01ad049c6a44600c394575a9989085cc8ca6a276
phlimasg <raphaelpc_@hotmail.com>
2019-09-05 12:08:59 -0300

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,13 +30,13 @@ class AppServiceProvider extends ServiceProvider
         $events->listen(BuildingMenu::class, function (BuildingMenu $event) {
             
             if(Gate::check('portal', Auth::user())){
-                $event->menu->add('DASHBOARD');
+                /*$event->menu->add('DASHBOARD');
                 $event->menu->add([
                 'text'        => 'Dashboard',
                 'url'         => route('communicated.index'),
                 'icon'        => 'home',
                 'can' => 'portal'
-                ]);
+                ]);*/
                 $event->menu->add('PAINEL DO RESPONSÁVEL');
                 $event->menu->add([
                 'text'        => 'Comunicados',
@@ -49,13 +49,7 @@ class AppServiceProvider extends ServiceProvider
                     'url'         => route('acesso.index'),
                     'icon'        => 'id-card',
                     'can' => 'portal'
-                    ]);
-                $event->menu->add([
-                    'text'        => 'Acesso/Catraca',
-                    'url'         => route('acesso.index'),
-                    'icon'        => '500px',
-                    'can' => 'portal2'
-                    ]);
+                    ]);                
             }
             //$event->menu->add('COMUNICADOS');
             if(Gate::check('editor', Auth::user())){
