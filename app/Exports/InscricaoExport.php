69acf4168f158b44f56b9cc328374c3d7942c4c8
phlimasg <raphaelpc_@hotmail.com>
2020-03-04 10:30:44 -0300

    Add turma no Relatório

--- app/Exports/InscricaoExport.php
+++ app/Exports/InscricaoExport.php
@@ -22,7 +22,7 @@ class InscricaoExport implements FromCollection
         $dt_fim = date('Y-m-d', strtotime(str_replace('/','-',$this->request->fim)));
         $inscricao = ExtInscricao::whereBetween('created_at',[$dt_ini.' 00:00:00',$dt_fim.' 23:59:59'])
         ->orderBy('ext_atv_turmas_id','asc')->orderBy('created_at','asc')->get();
-        $export[] = ['RA','NOME_ALUNO','ATIVIDADE','TURMA','HORA DA INSCRICAO','VALOR'];
+        $export[] = ['RA','NOME_ALUNO','ATIVIDADE','TURMA','TURMA DO ALUNO','HORA DA INSCRICAO','VALOR'];
         
         foreach ($inscricao as $i) {           
             $export[]=[                
@@ -30,6 +30,7 @@ class InscricaoExport implements FromCollection
                 'NOME_ALUNO' => $i->aluno->NOME_ALUNO,
                 'ATIVIDADE' => $i->ExtAtvTurma->ExtAtv->atividade,
                 'TURMA' => $i->ExtAtvTurma->descricao_turma,
+                'TURMA DO ALUNO' => $i->aluno->TURMA,
                 'HORA DA INSCRICAO' => $i->created_at,
                 'VALOR' => 'R$ '.substr_replace($i->amount,',',-2,0),
             ];
