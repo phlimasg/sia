3225ed6a1a6ee785f18e4bf8e86cea99e70867bd
phlimasg <raphaelpc_@hotmail.com>
2020-03-09 17:20:30 -0300

    Update InscricaoExport.php

--- app/Exports/InscricaoExport.php
+++ app/Exports/InscricaoExport.php
@@ -22,7 +22,7 @@ class InscricaoExport implements FromCollection
         $dt_fim = date('Y-m-d', strtotime(str_replace('/','-',$this->request->fim)));
         $inscricao = ExtInscricao::whereBetween('created_at',[$dt_ini.' 00:00:00',$dt_fim.' 23:59:59'])
         ->orderBy('ext_atv_turmas_id','asc')->orderBy('created_at','asc')->get();
-        $export[] = ['RA','NOME_ALUNO','ATIVIDADE','TURMA','TURMA DO ALUNO','HORA DA INSCRICAO','VALOR'];
+        $export[] = ['RA','NOME_ALUNO','ATIVIDADE','TURMA','TURMA DO ALUNO','HORA DA INSCRICAO','VALOR','RESPONSAVEL','EMAIL'];
         
         foreach ($inscricao as $i) {           
             $export[]=[                
@@ -33,6 +33,8 @@ class InscricaoExport implements FromCollection
                 'TURMA DO ALUNO' => $i->aluno->TURMA,
                 'HORA DA INSCRICAO' => $i->created_at,
                 'VALOR' => 'R$ '.substr_replace($i->amount,',',-2,0),
+                'RESPONSAVEL'=> $i->aluno->RESPACAD,
+                'EMAIL'=> $i->aluno->RESPACADEMAIL,
             ];
         }
         $export = new Collection($export);
