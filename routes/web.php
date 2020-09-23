354685bd21a71bde1aef2c38bee4787776b4a29c
phlimasg <raphaelpc_@hotmail.com>
2020-09-23 18:01:20 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@
 Route::get('/', function () {
     return redirect()->route('portal.index');
 });
-Route::get('https://sia.abel.org.br/bolsa_social/renovacao', function(){
-    return redirect('http://sbd.lasalle.org.br/colegio-la-salle-abel/renova%C3%A7%C3%A3o-de-bolsa-social');
+Route::get('bolsa_social/renovacao', function(){
+    return redirect()->url('http://sbd.lasalle.org.br/colegio-la-salle-abel/renova%C3%A7%C3%A3o-de-bolsa-social');
 });
 Route::post('senderror', 'Mail\MailController@senderror')->name('senderror');
 
