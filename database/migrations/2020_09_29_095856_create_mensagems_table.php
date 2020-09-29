ae49709274e282b186c1de58a95defa42c12e3b7
phlimasg <raphaelpc_@hotmail.com>
2020-09-29 12:50:57 -0300

    Update 2020_09_29_095856_create_mensagems_table.php

--- database/migrations/2020_09_29_095856_create_mensagems_table.php
+++ database/migrations/2020_09_29_095856_create_mensagems_table.php
@@ -14,7 +14,7 @@ class CreateMensagemsTable extends Migration
     public function up()
     {
         Schema::create('mensagems', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
             $table->text('mensagem');
             $table->unsignedBigInteger('CANDIDATO_ID');               
             $table->foreign('CANDIDATO_ID')
