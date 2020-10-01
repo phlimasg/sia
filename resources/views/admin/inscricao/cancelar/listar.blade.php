e9f73c4e5d01057cd393214fb45541b8053e2911
phlimasg <raphaelpc_@hotmail.com>
2020-10-01 16:20:56 -0300

    Update listar.blade.php

--- resources/views/admin/inscricao/cancelar/listar.blade.php
+++ resources/views/admin/inscricao/cancelar/listar.blade.php
@@ -11,7 +11,7 @@
 <div class="alert alert-warning alert-dismissible">
   <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
   <h4><i class="icon fa fa-warning"></i> Aviso!</h4>
-  {{dd(Session::get('message'))}}
+  {{Session::get('message')}}
 </div>
 @endif
 <div class="box">
