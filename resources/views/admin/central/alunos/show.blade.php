c7d3c916e38597af1ee4a69bad62f049c074f519
phlimasg <raphaelpc_@hotmail.com>
2021-02-11 10:28:02 -0300

    Update show.blade.php

--- resources/views/admin/central/alunos/show.blade.php
+++ resources/views/admin/central/alunos/show.blade.php
@@ -5,6 +5,30 @@
 @section('content')
 
 <div class="row">
+  <div class="col-md-12">
+    <div class="box box-primary">
+      <div class="box-header">
+        <h3 class="box-title">Dados do Aluno</h3>
+  
+        <div class="box-tools">
+          <form action="{{ route('alunos_matriculados.store') }}" method="post">
+            @csrf
+            <div class="input-group input-group-sm hidden-xs" style="width: 250px;">
+              <input type="text" name="table_search" class="form-control pull-right" placeholder="Procurar" value="{{old('table_search')}}">
+    
+              <div class="input-group-btn">
+                <button type="submit" class="btn btn-default"><i class="fa fa-search"></i></button>
+              </div>
+            </div>
+          
+          </form>
+        </div>
+      </div>
+      <!-- /.box-header -->
+    </div>
+  </div>
+</div>
+<div class="row">
   <div class="col-md-3">
     <div class="box box-primary">
       <div class="box-body box-profile">
