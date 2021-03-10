a314a1258f186204cc53668fd3ae9e2a2ce3d4f5
phlimasg <raphaelpc_@hotmail.com>
2021-03-10 11:53:06 -0300

    Update GetnetController.php

--- app/Http/Controllers/GetnetController.php
+++ app/Http/Controllers/GetnetController.php
@@ -413,8 +413,9 @@ class GetnetController extends Controller
                           
         } catch (RequestException  $e) {
             //$e->getRequest()
-            dd($e->getResponse());
+            dd($e,$e->getResponse(),$e->getResponse()->getBody());
             $e = json_decode($e->getResponse(),true);
+            $e = json_decode($e->getResponse()->getBody(),true);
             //dd($error);
             return view('errors.error', compact('e'));
             //return redirect()->back()->with('error',$error);
